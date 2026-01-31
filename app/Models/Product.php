1ab0d9628cf006286695648e4e07fdd274d3c86b
adnane el yazidi <adanyazi22@outlook.com>
2026-01-31 14:42:29 +0100

    feat(products): add Product model with fillable fields and category relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['name', 'slug', 'reference', 'description_short', 'price', 'stock', 'image', 'category_id'];
+
+    public function category(): BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
 }
